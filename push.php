9ed25cca3ccf4634ee78d9337cebfe6b42b2f6f0
Xavier <xw@flaptop.app.lighting>
2019-11-21 11:35:33 -0500

    Update webhook 'push.php' to be used on AWS. On remote push: AWS does pull

--- push.php
+++ push.php
@@ -1,7 +1,4 @@
 <?php
-echo "{\n";
-foreach( $_POST as $key => $value ) {
-	 echo "$key : $value,\n";
-}
-echo "\n};"
+$result = exec( "git pull" );
+echo "Pulled repo with shell result:" .  $result ;
 ?>
