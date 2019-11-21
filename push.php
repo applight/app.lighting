d5debf459357c1c567838c79c91eb0e948066fb0
Admin <admin@app.lighting>
2019-11-21 11:47:37 -0500

    Update push.php with fully qualified path of webserver

--- push.php
+++ push.php
@@ -1,4 +1,9 @@
 <?php
-$result = exec( "git pull" );
+
+// cd to the fully qualified path (htdocs is a symlink,
+// just in case we change the server later)
+// then do a git pull
+$result = exec( "cd /home/bitnami/htdocs ; git pull" );
+
 echo "Pulled repo with shell result:" .  $result ;
 ?>
