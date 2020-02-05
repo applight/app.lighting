50f4ccc4301fb7b427238a542e82ce2b57acea53
Ubuntu <ubuntu@ip-172-26-2-53.ec2.internal>
2020-02-05 13:46:34 +0000

    Remove xmattv.com

--- xmattv/nav.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "<nav id=\"menu\">" .
-    "<ul class=\"links\">" .
-    "<li><a href=\"index.php\">Home</a></li>" .
-    "<li><a href=\"resume.php\">Resume</a></li>" .
-    "<li><a href=\"assistant.php\">Twilio Assistant</a></li>" .
-    "</ul></nav>";
-?>
