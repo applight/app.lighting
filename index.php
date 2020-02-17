2cc77bd122ea63d06febf8c21fc96e33a209a417
Admin <admin@app.lighting>
2020-02-17 16:53:11 -0500

    index update

--- index.php
+++ index.php
@@ -97,22 +97,22 @@ function load_main( $page ) {
                                 <div class="contact-method">
                                     <span class="icon solid alt fa-envelope"></span>
                                     <h3>Email</h3>
-                                    <a href="#">information@untitled.tld</a>
+                                    <a href="mailto:info@app.lighting">info@app.lighting</a>
                                 </div>
                             </section>
                             <section>
                                 <div class="contact-method">
                                     <span class="icon solid alt fa-phone"></span>
                                     <h3>Phone</h3>
-                                    <span>(000) 000-0000 x12387</span>
+                                    <span>(888) 200-1601 x9</span>
                                 </div>
                             </section>
                             <section>
                                 <div class="contact-method">
                                     <span class="icon solid alt fa-home"></span>
                                     <h3>Address</h3>
-                                    <span>1234 Somewhere Road #5432<br />
-                                    Nashville, TN 00000<br />
+                                    <span>App Lighting - 58 Lubek St<br />
+                                    East Boston, MA 02138<br />
                                     United States of America</span>
                                 </div>
                             </section>
