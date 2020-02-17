ff1b03d6da2b691c64419291b8893432763d90b1
Admin <admin@app.lighting>
2020-02-17 15:36:33 -0500

    fix vendor path

--- wireless/transcription-to-test.php
+++ wireless/transcription-to-test.php
@@ -1,5 +1,5 @@
 <?php
-require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '../vendor/autoload.php';
 // Update the path below to your autoload.php,
 // see https://getcomposer.org/doc/01-basic-usage.md
 use Twilio\Rest\Client;
