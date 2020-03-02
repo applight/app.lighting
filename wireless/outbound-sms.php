d96de4a7f75784fe9f091128df8aad69a07bb6ce
Admin <admin@app.lighting>
2020-03-02 15:50:10 -0500

    add outboud-sms php script

---
+++ wireless/outbound-sms.php
@@ -0,0 +1,25 @@
+<?php
+require_once '/var/www/vhosts/app.lighting/httpdocs/vendor/autoload.php';
+
+use Twilio\TwiML;
+use Twilio\TwiML\MessagingResponse;
+
+function desimify( $from ) {
+         switch ( trim($from) ) {
+                case "sim:DEdec7c449c69d576bd67a434bc92954e0":
+                     return "+16173345281";
+                     break;
+                default:
+                     return trim( $from );
+                     break;
+         }
+}
+
+$resp = new MessageResponse();
+$msg = $resp->message( $_POST['Body'] );
+$msg->to( $_POST['To'] );
+$msg->from( desimify( $_POST['From'] ) );
+
+echo $resp;
+
+?>
