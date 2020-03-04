8740ae663c81ec57099c9c74bad04f2838820c72
applight <56211536+applight@users.noreply.github.com>
2020-03-04 02:33:33 -0500

    Create libAppLighting.php
    To be used for repeat use primitive functions

---
+++ wireless/libAppLighting.php
@@ -0,0 +1,12 @@
+<?php
+function desimify( $from ) {
+         switch ( trim($from) ) {
+                case "sim:DEdec7c449c69d576bd67a434bc92954e0":
+                     return "+16173345281";
+                     break;
+                default:
+                     return trim( $from );
+                     break;
+         }
+}
+?>
