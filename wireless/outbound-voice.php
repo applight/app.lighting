bd1ac5f0820784a0a69a34a40b375b7c44e129ac
Admin <admin@app.lighting>
2020-03-08 19:51:10 -0400

    redo outbound-voice to not require prompts

--- wireless/outbound-voice.php
+++ wireless/outbound-voice.php
@@ -8,55 +8,29 @@ use Twilio\TwiML\VoiceResponse;
 
 $to     = trim( $_POST['To'] );
 $from   = trim( $_POST['From'] );
-$digits = trim( $_POST['Digits'] );
 
-if ( $digits ) {
-// second pass, we have digits entered
-   switch ( $digits ) {
-   case "0":
-       // if it's me
-       if ( strncmp($from, "mvaughan@applight.sip.us1.twilio.com", 36) == 0 ) {
-           $from = "+16173345281";
-       } else {
-           $from = "+16173351304";
-       }      
-       break;
-   case "9":
-       // do a real random pull later, for now just 888
-       $from = "+18882001601";
-       break;
-   default:
-       if ( strlen( $digits ) == 10 ) $from = "+1" . $digits;
-       break;
-   }
-   
-   echo '<?xml version="1.0" encoding="UTF-8"?><Response><Dial answerOnBridge="true" callerId="' . $from  . '"><Number>' . $to . '</Number></Dial></Response>';
-
-} else {
-    $to = $rawTo = $_POST['To'];
-    $callerId = $_POST['From'];
-
-    // parse 'callerId' number from dailed
-    $tos = explode("*", trim($rawTo), 2);
-    if ( count($tos) == 2 ) {
-        $callerId = $tos[0];
-        $to       = $tos[1];
-    }
-   // First pass through -- gather has not had a response
-   $response = new VoiceResponse();
-
-   $dial = $response->dial( ['callerId' => $callerId,
-                             'answerOnBridge' => 'true' ] );
-   $dial->number( $to );
-
-   echo $response;
-   return;
-   
-   $gather = $response->gather([ 'input' => 'DTMF speech', 'numDigits' => 10 ]);
-   $gather->say('enter the number to appear on caller eye dee, or press zero pound for default, or nine pound for a proxy number');
-   
-   echo $response;
-      
+// correct sip 'From' so it will work with PTSN
+if ( strncmp($from, "mvaughan@applight.sip.us1.twilio.com", 36) == 0 ) {
+    $from = "+19783879792";
 }
+
+// add the "+1" to numbers without it
+if ( "+1" != substr( $to, 0, 2 ) ) {
+    $to = "+1" . $to;
+}
+
+// assume numbers of length 12 are just regular US numbers
+// for strings of a greater length assume anything after the 12 is the from
+if ( strlen($to) != 12 ) {
+    $from = substr( $to, 12 );
+    $to   = substr( $to, 0, 12 );
+}
+
+$response = new VoiceResponse();
+
+$dial = $response->dial( ['callerId' => $from,
+                          'answerOnBridge' => 'true' ] );
+$dial->number( $to );
+echo $response;
 
 ?>
