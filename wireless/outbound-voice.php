f6a721ef3a99cb14a773363b0cd267c44693a0ea
Admin <admin@app.lighting>
2020-03-08 20:06:51 -0400

    use length not strlen

--- wireless/outbound-voice.php
+++ wireless/outbound-voice.php
@@ -9,9 +9,11 @@ use Twilio\TwiML\VoiceResponse;
 $to     = trim( $_POST['To'] );
 $from   = trim( $_POST['From'] );
 
-// correct sip 'From' so it will work with PTSN
+// correct sip 'From' or sim 'From' so it will work with PTSN
 if ( strncmp($from, "mvaughan@applight.sip.us1.twilio.com", 36) == 0 ) {
     $from = "+19783879792";
+} elseif ( strncmp($from, "sim:DEdec7c449c69d576bd67a434bc92954e0", 38) == 0 ) {
+    $from = "+16173345281";
 }
 
 // add the "+1" to numbers without it
@@ -21,7 +23,7 @@ if ( "+1" != substr( $to, 0, 2 ) ) {
 
 // assume numbers of length 12 are just regular US numbers
 // for strings of a greater length assume anything after the 12 is the from
-if ( strlen($to) != 12 ) {
+if ( length($to) != 12 ) {
     $from = substr( $to, 12 );
     $to   = substr( $to, 0, 12 );
 }
