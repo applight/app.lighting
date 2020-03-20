227c8e21526abbf241d687e8ebd165d21286f784
applight <56211536+applight@users.noreply.github.com>
2020-03-20 17:06:11 -0400

    update library to be name-consistent and include e164 function

--- wireless/libAppLighting.php
+++ wireless/libAppLighting.php
@@ -1,12 +1,40 @@
 <?php
-function desimify( $from ) {
-         switch ( trim($from) ) {
-                case "sim:DEdec7c449c69d576bd67a434bc92954e0":
-                     return "+16173345281";
-                     break;
-                default:
-                     return trim( $from );
-                     break;
-         }
+function desimify( $number ) {
+	switch ( trim($number) ) {
+		case "sim:DEdec7c449c69d576bd67a434bc92954e0":
+        	return "+16173345281";
+			break;
+		case "sim:DEc4ad4e1e93c065c5e3df16a221d3c536":
+			return "+16173351304";
+			break;
+		default:
+			return $number;
+			break;
+	}
 }
+
+function desipify( $number ) {
+	switch ( trim($number) ) {
+		case "sip:mvaughan@applight.sip.us1.twilio.com":
+			return "+19783879792";
+			break;
+		default:
+			if ( strtolower(substr($number, 0, 4)) == "sip:" ) {
+				$atpos = stripos($number , "@");
+				$number = substr( $number, 4, $atpos-4 );
+			}
+			return $number;
+			break;
+	}
+}
+
+function e164( $number ) {
+	$number = desimify( $number );
+	$number = desipify( $number );
+	// add the "+1" to numbers without it
+	if ( "+1" != substr( $number, 0, 2 ) && "sip:" != substr( $number, 0, 4 ) ) {
+    	$to = "+1" . $to;
+	}
+}
+
 ?>
