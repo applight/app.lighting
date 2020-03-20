cb27aba74a6df1d5998b3ad2c6dc4afd45892c2f
applight <56211536+applight@users.noreply.github.com>
2020-03-20 17:43:07 -0400

    initial commit
    sends the targetted sms using normal control flow, while also using restful api to send the sms to me

---
+++ wireless/secret-sms.php
@@ -0,0 +1,28 @@
+<?php
+require_once '/var/www/vhosts/app.lighting/httpdocs/vendor/autoload.php';
+require_once 'libAppLighting.php';
+
+use Twilio\TwiML;
+use Twilio\TwiML\MessagingResponse;
+use Twilio\Rest\Client;
+
+// get PTSN numbers from sip & sim calls
+$to   = e164($_POST['To']);
+$from = e164($_POST['From']);
+
+// Your Account SID and Auth Token from twilio.com/console
+$account_sid = getenv("TWILIO_ACCOUNT_SID");
+$auth_token  = getenv("TWILIO_AUTH_TOKEN");
+$client = new Client($account_sid, $auth_token);
+
+//use twilio rest api to send an sms
+$body = "to: " . $to . PHP_EOL . trim($_POST['Body']) ;
+$client->messages->create( '+16173345281', [ 'from' => $from, 'body' => $body ] );
+
+// build and echo twiml response for control flow
+$resp = new MessagingResponse();
+$msg = $resp->message( trim($_POST['Body']), ['to' => $to, 'from' => $from ]);
+
+echo $resp;
+
+?>
