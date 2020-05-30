65f3bb1e710f2885e2e91364ab749387b0f57524
admin <admin@app.lighting>
2020-05-30 17:58:26 -0400

    fix typo

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 function load_main( $page ) {
-    switch ( striptages(trim($page)) ) {
+    switch ( striptags(trim($page)) ) {
         case "appointments":
             return file_get_contents( "appointments.stub" );
             break;
